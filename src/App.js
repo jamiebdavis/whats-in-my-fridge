5a147c6f1bc5b534aa462415536192f28e348de6
jamie davis <jamied1985@gmail.com>
2021-06-27 18:07:37 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/navbar/navbar";
+
+function App() {
+  return (
+    <div className="m-2">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
+
+const Title = () => <h1 className="text-red-200	">What's in my fridge</h1>;
